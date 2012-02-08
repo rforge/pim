8f12bfc4be04392c3b9c45a63182ae2b3cc23f5c
jorismeys <jorismeys@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-08 18:36:43 +0000

    Corrected DESCRIPTION and NAMESPACE to make build possible Corrected argument used in plot.pim.R Added a comment in PIM.Solve.R Changed the pim.Rd file in order to remove error generated by \abs command. See comment in file.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pim@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pim/R/PIM.Solve.R
+++ pkg/pim/R/PIM.Solve.R
@@ -1,7 +1,7 @@
 PIM.Solve <-
 function(Z, PO, linkfunction)
 {
-	if (!exists("nleqslv")) require(nleqslv)
+	if (!exists("nleqslv")) require(nleqslv) # JM : when an import is specified, no need to do this
 	if(linkfunction == "logit" | linkfunction == "probit") 
 		fit.x <- nleqslv(rep(0,ncol(Z)), PIM.ScoreFunction(Z, PO, linkfunction), Z=Z, PO=PO)$x
 	else stop("linkfunction must be 'logit' or 'probit'.")
--- pkg/pim/R/plot.pim.R
+++ pkg/pim/R/plot.pim.R
@@ -1,12 +1,13 @@
 plot.pim <-
-function(object, G=10, ...)
+function(x, G=10, ...)
 {
-	fv.tmp <- fitted.values(object)
+	fv.tmp <- fitted.values(x)
 	groups.tmp <- cut(fv.tmp, quantile(fv.tmp, probs =   seq(0,1, 1/G)), include.lowest = T) 
-	emp.prob <- tapply(object$PIMdata$PO, groups.tmp, mean)
+	emp.prob <- tapply(x$PIMdata$PO, groups.tmp, mean)
 	model.prob <- tapply(fv.tmp, groups.tmp, mean)
 	plot(emp.prob, model.prob, xlim=c(min(emp.prob),max(emp.prob)) ,ylim=c(min(model.prob),max(model.prob)),main="",lwd=2,xlab="",ylab="")
 	title(xlab="Empirical PI",ylab="Model PI",font.lab=1,cex.lab=1,main="Goodness-of-fit")
 	abline(0,1,col=2)
 }
 
+# JM : changed 'object' to 'x' as to comply to S3 inheritance rules
