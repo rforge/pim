b026ab1568145236f73a3da3f3855c87d671b180
jorismeys <jorismeys@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-08-25 11:22:29 +0000

    Updated package to reflect name change
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pim@35 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/pimdevel/R/pimdevel-package.R
@@ -0,0 +1,18 @@
+#' Probabilistic Index Models
+#' 
+#' Fit a probabilistic index model. This package contains quite different functionality, and is meant for development of new methods. For standard analysis, please use the pim package instead of the pimdevel package.
+#' 
+#' \tabular{ll}{ Package: \tab pimdevel\cr Type: \tab Package\cr Version: \tab
+#' 1.1.5.4\cr Date: \tab 2013-08-03\cr License: \tab GPL (>= 2)\cr
+#' LazyLoad: \tab yes\cr Depends: R (>= 2.12), Matrix, nleqslv, MASS\cr Suggests: glmnet (>= 1.9-3), BB, lqa} 
+#' @name pimdevel-package
+#' @aliases pimdevel-package
+#' @docType package
+#' @author Nick Sabbe \email{Nick.Sabbe@@UGent.be} Jan De Neve \email{JanR.DeNeve@@UGent.be} Joris Meys \email{Joris.Meys@@UGent.be}
+#' @references [PENDING]
+#' @keywords package
+#' @import Matrix nleqslv MASS 
+NULL
+
+
+
