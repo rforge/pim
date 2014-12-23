973b9cf6cdba94b25b9bdcd7fd5318c4fc63ba0d
jorismeys <jorismeys@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-23 18:46:33 +0000

    Updated help file of add.poset for future reference
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pim@52 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pimdevel/R/add.poset.R
+++ pkg/pimdevel/R/add.poset.R
@@ -3,13 +3,23 @@
 #' This function adds a poset to a \code{\link{pim.environment}}
 #' object.
 #' 
+#' TO DO: CHECK IDEA AND IMPLEMENT FURTHER (control of nobs)
+#' 
 #' @param x a pim.environment object
 #' @param overwrite a logical value indicating whether the poset
 #' should be overwritten if it's already present. Defaults to 
 #' \code{FALSE} to avoid problems.
 #' @param ... further parameters passed to \code{\link{new.pim.poset}}.
-#' Note that you have to provide at least a value for the argument 
-#' \code{nobs}. 
+#' 
+#' @section Warning: Although it might be tempting to set the argument
+#' \code{nobs}, you shouldn't. The necessary information is taken
+#' from the respective slot in the \code{pim.environment} object.
+#' 
+#' If you provide a matrix or a list as value for the argument \code{compare},
+#' note that you can easily create a poset that doesn't use all the 
+#' observations. This might or might not be your intention. If the poset
+#' you try to create contains indices that go beyond the number of
+#' observations, an error is thrown.
 #' 
 #' @return The object with a (new) poset attached.
 #' @seealso \code{\link{new.pim.poset}} for the possible values of the
@@ -26,5 +36,6 @@ setMethod('add.poset',
             if(is.complete(x) & !overwrite){
               stop('pim.environment has already a poset attached. Set overwrite to FALSE if you want to replace the current one.')
             }
-            x@poset <- new.pim.poset()
+            x@poset <- new.pim.poset(...)
+            x
           })
