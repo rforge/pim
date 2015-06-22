180d635dc25d0a275b2b8f8aa52d55dd7839900c
jorismeys <jorismeys@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-06-22 15:32:38 +0000

    summary work
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pim@80 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pimdevel/R/pim.summary-class.R
+++ pkg/pimdevel/R/pim.summary-class.R
@@ -4,7 +4,18 @@
 #' model , and is created by using the function \code{\link{summary}}
 #' on an object of the \code{\link{pim-class}}.
 #' 
-#' TO BE FINISHED
+#' The class \code{pim.summary} can be treated like a matrix to get out
+#' the coefficients, standard errors, Z values and/or p values. 
+#' 
+#' @slot formula contains an object of the class \code{\link{pim.formula}}
+#' containing the model fitted.
+#' 
+#' @slot coef a numeric vector with the coefficients
+#' @slot se a numeric vector with the standard errors for the coefficients
+#' @slot zval a numeric vector containing the Z values for the coefficients,
+#' testing whether the coefficient differs significantly from 0.
+#' @slot pr a numeric vector containing the related p-values for the
+#' coefficients.
 #' 
 #' @seealso \code{\link{pim}} for more info on how to construct the model
 #' 
