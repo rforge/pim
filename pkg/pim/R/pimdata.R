236da3dc9cc3abde7d36bbfdf4c83df3e94e846e
jorismeys <jorismeys@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-06-27 13:05:45 +0000

    Changed a few things on the examples of the pim page to reduce calculation time
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pim@148 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pim/R/pimdata.R
+++ pkg/pim/R/pimdata.R
@@ -7,7 +7,7 @@
 #' @details The data contained in the package has following structures
 #' \itemize{
 #'   \item{\code{\link{EngelData}}}: A single numeric predictor variable and a response
-#'   \item{\code{\link{FEVData}}}: 
+#'   \item{\code{\link{FEVData}}}: A data frame with a numeric response variable and 4 additional numeric predictor values.
 #' 	\item{\code{\link{DysData}}}: A dataframe with 3 factors and a numeric variable as predictors. The outcome is a factor with 4 levels.
 #' 	\item{\code{SNP_XRCC1__77}} Genotype of this SNP. A factor with three levels: "TT", "TC" and "CC"
 #' 	\item{\code{SPC_D2}} Dose of radiation that reached 2% of the SPC muscle
